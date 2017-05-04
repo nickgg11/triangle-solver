da0cfd6613021238ead83b2f0083c67f9bbca842
MrZebarth <kris.zebarth@ocsb.ca>
2017-05-04 14:11:15 -0400

    Added outline for Trig Package

---
+++ src/trig/Trig.java
@@ -0,0 +1,20 @@
+package trig;
+
+public class Trig {
+
+	public double sinLawSide(double A, double a, double B){
+		
+	}
+	
+	public double sinLawAngle(double A, double a, double b){
+		
+	}
+	
+	public double cosLawSide(double A, double b, double c){
+		
+	}
+	
+	public double cosLawAngle(double a, double b, double c){
+		
+	}
+}
