8302fd0b4e8de198886d1a885160d0d0b5c163fb
nickgg11 <nicholas.gingras@ocsbstudent.ca>
2017-05-17 13:07:45 -0400

    added  full triangle solver

--- src/trig/test.java
+++ src/trig/test.java
@@ -1,20 +1,11 @@
 package trig;
 import java.util.Scanner;
 public class test {
-
-	public double sides2Angles1(double a, double b, double A)	{
-		Trig triggered=new Trig();
-		double B=triggered.cosLawAngle(a, b, A);
-		double C=180-A-B;
-		double c=triggered.sinLawSide(A ,a ,B);
-		System.out.println("angles are"+A+" , "+B+" , "+C+" , The side lengths are "+a+" , "+b+" , "+c);
-	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Trig triggered=new Trig();
 		int choice=0;
-		
+
 		do{
 			System.out.println("\nChoose which math you want to do\n1. Sine Law for a side length\n2. Sine law for an angle\n3. Cosine law for a side length\n4. Cosine law for an angle\n5. Solve full Triangle");
 			Scanner in= new Scanner(System.in);
@@ -74,9 +65,103 @@ public class test {
 				B=in.nextInt();
 				System.out.println("angle C");
 				double C=in.nextInt();
-				 Sides2Angles1;
+				in.nextLine();
+				do
+				{
+					if (a==0)
+					{
+						if (A>0 && B>0 && b>0)
+						{
+							a=triggered.sinLawSide(A,b,B);
+						}
+						else if (A>0 && C>0 && c>0)
+						{
+							a=triggered.sinLawSide(A, c, C);
+						}
+						else if (A>0 && b>0 && c>0)
+						{
+							a=triggered.cosLawSide(A, b, c);
+						}
+					}
+					else if (b==0)
+					{
+						if (A>0 && a>0 && B>0)
+						{
+							b=triggered.sinLawSide(A, a, B);
+						}
+						else if (C>0 && c>0 && B>0)
+						{
+							b=triggered.sinLawSide(C, C, B);
+						}
+						else if (B>0 && a>0 && c>0)
+						{
+							b=triggered.cosLawSide(B, a, c);
+						}
+					}
+					else if (c==0)
+					{
+						if (C>0 && a>0 && A>0)
+						{
+							c=triggered.sinLawSide(C, a, A);
+						}
+						else if (C>0 && b>0 && B>0)
+						{
+							c=triggered.sinLawSide(C, b, B);
+						}
+						else if (C>0 && a>0 && b>0)
+						{
+							c=triggered.cosLawSide(C, a, b);
+						}
+					}
+					else if (A==0)
+					{
+						if (a>0 && b>0 && B>0)
+						{
+							A=triggered.sinLawAngle(a, b, B);	
+						}
+						else if (a>0 && c>0 && C>0)
+						{
+							A=triggered.sinLawAngle(a, c, C);
+						}
+						else if  (a>0 && b>0 && c>0)
+						{
+							A=triggered.cosLawAngle(a, b, c);
+						}
+					}
+					else if (B==0)
+					{
+						if (b>0 && a>0 && A>0)
+						{
+							B=triggered.sinLawAngle(A, b, a);
+						}
+						else if (b>0 && c>0 && C>0)
+						{
+							B=triggered.sinLawAngle(C, c, b);
+						}
+						else if (a>0 && b>0 && c>0)
+						{
+							B=triggered.cosLawAngle(c, b, a);
+						}
+					}
+					else if (C==0)
+					{
+						if (c>0 && a>0 && A>0)
+						{
+							C=triggered.sinLawAngle(A, c, a);	
+						}
+						else if (c>0 && b>0 && B>0)
+						{
+							C=triggered.sinLawAngle(B, c, a);
+						}
+						else if (a>0 && b>0 && c>0)
+						{
+							C=triggered.cosLawAngle(a, b, c);
+						}
+					}
+				} while (a==0 || b==0 || c==0 || A==0 || B==0 || C==0);
+				System.out.println("Okay your sides and angles are \nSide a: "+a+"\nSide b: "+b+"\nSide c: "+c+"\nAngle A: "+A+"\nAngle B:"+B+"\nAngle C: "+C);
 			}
-			}
+		}
 		while(choice==1||choice==2||choice==3||choice==4||choice==5);
 		System.out.println("YOU LEFT NOW YOU HAVE TO START AGAIN.");
 	}
